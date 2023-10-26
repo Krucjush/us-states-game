a28ebb037a5750168ae1f72122c6d61b236922fa
Dawid Studzizba <studzizba.dawid@gmail.com>
2023-10-26 20:58:00 +0200

    Clean code

--- main.py
+++ main.py
@@ -20,10 +20,7 @@ while len(correct_guesses) < 50:
                                     prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in states:
-            if state not in correct_guesses:
-                missing_states.append(state)
+        missing_states = [state for state in states if state not in correct_guesses]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
